446c2873e2b8c3b1925f4b99937df618a7b5f1e6
Jeremiah Montoya <thisjeremiah@users.noreply.github.com>
2022-08-13 13:15:54 -0700

    color exploration

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -5,7 +5,7 @@ import Title from '../components/Title'
 
 const Schedule: NextPage = () => {
   return (
-    <Layout className="bg-cobalt-400 text-lemon-400">
+    <Layout className="bg-berry-600 text-lemon-400">
       <div className="mx-8 text-center h-full">
         <Title className="pb-20">Schedule</Title>
         <div className="absolute inset-0 flex justify-center items-center pointer-events-none">
@@ -14,7 +14,7 @@ const Schedule: NextPage = () => {
               <h2 className="font-serif text-xl">Friday, May 19, 2023</h2>
               <p className="lowercase text-lg">Rehearsal Dinner</p>
             </div>
-            <div className="text-rose-600 flex justify-center gap-1">
+            <div className="text-lemon-500 flex justify-center gap-1">
               <Tile type="flower" />
               <Tile type="hibiscus" />
               <Tile type="flower" />
