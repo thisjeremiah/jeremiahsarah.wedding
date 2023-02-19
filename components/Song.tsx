e35b297251d9d0b30f2fb66ad17b4d505af474c3
Jeremiah Montoya <thisjeremiah@users.noreply.github.com>
2023-02-19 15:08:23 -0800

    use icons

--- components/Song.tsx
+++ components/Song.tsx
@@ -45,13 +45,11 @@ export function Song({
           <div className="h-full flex items-center justify-center w-full absolute">
             <button
               className={cx(
-                'font-serif cursor-pointer transition text-white',
-                isPlaying
-                  ? 'text-2xl'
-                  : 'text-lg group-hover:opacity-100 sm:opacity-0',
+                'cursor-pointer transition text-white',
+                isPlaying ? '' : 'group-hover:opacity-100 sm:opacity-0',
               )}
             >
-              {isPlaying ? '⏸' : '▶'}
+              {isPlaying ? <PauseIcon /> : <PlayIcon />}
             </button>
           </div>
           {isPlaying ? (
@@ -96,5 +94,39 @@ export function Song({
         </div>
       )}
     </div>
+  )
+}
+
+function PauseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-5 h-5 select-none"
+    >
+      <path
+        fillRule="evenodd"
+        d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+}
+
+function PlayIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-5 h-5 select-none"
+    >
+      <path
+        fillRule="evenodd"
+        d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+        clipRule="evenodd"
+      />
+    </svg>
   )
 }
